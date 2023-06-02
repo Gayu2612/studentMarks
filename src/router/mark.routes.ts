7ffc2154cba841ca7cd2d4623289ba2bd2f3e546
gayathri <gayathri@upturntechnology.com>
2023-06-02 17:48:58 +0530

    ooo

--- src/router/mark.routes.ts
+++ src/router/mark.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import { getAllStudentMarks, saveStudentMarks } from '../controller/marks.controller';
+import { deleteStudentMarks, getAllStudentMarks, saveStudentMarks, updateStudentMarks } from '../controller/marks.controller';
 
 const router: Router = Router();
 
 
 router.post('/', saveStudentMarks)
 router.get('/', getAllStudentMarks)
-// router.put('/', updateStudent)
-// router.delete('/', deleteStudent)
+router.put('/', updateStudentMarks)
+router.delete('/', deleteStudentMarks)
 // router.get('/getSingleStudent', getSingleStudent)
 
 export default router;
